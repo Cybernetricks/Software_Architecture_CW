99406d76524e569ed90263e61ccaccc13b769f06
Cybernetricks <mcgbradley@gmail.com>
2024-12-29 16:08:35 +0000

    implemented_manager_class

--- src/Depot/Manager.java
+++ src/Depot/Manager.java
@@ -1,5 +1,103 @@
 package Depot;
 
-public class Manager {
+import java.io.BufferedReader;
+import java.io.FileReader;
 
+public class Manager {
+  private Worker worker = new Worker();
+  private ParcelMap parcelMap = new ParcelMap();
+  private Log log = new Log(100);
+  private Gui gui = new Gui(this);
+
+  public Manager() {}
+
+  public void initiateCustomerQueue(String filename)
+  {
+    try{
+      FileReader file_Reader = new FileReader(filename);
+      BufferedReader buffered_Reader = new BufferedReader(file_Reader);
+      String line;
+      while(buffered_Reader.ready()){
+          line = buffered_Reader.readLine();
+          String[] array = line.split(",");
+          worker.addCustomerToQueue(new Customer(array[0], array[1].trim()));
+       } // end of outer while loop
+      buffered_Reader.close();
+      addToEventLog("Customer Queue has been initialised from file: " + filename);
+    }catch(Exception e){
+      e.printStackTrace();
+    }
+  }
+
+  public boolean addCustomerToQueue(Customer customer)
+  {
+    if (parcelMap.getParcel(customer.getParcelId()) == null) {
+      addToEventLog("Customer's Parcel " + customer.getParcelId() + " does not exist");
+      return false;
+    } else {
+      worker.addCustomerToQueue(customer);
+    }
+    return true;
+  }
+
+  public float processCurrentCustomerInQueue()
+  {
+    Parcel parcel = parcelMap.getParcel(worker.getCurrentCustomerParcelId());
+    parcelMap.changeParcelState(parcel.getId());
+    float collectionFee = worker.processCurrentCustomerInQueue(parcel);
+    addToEventLog("Parcel with ID " + parcel.getId() + " has been collected");
+    return collectionFee;
+  }
+
+  public String customerQueueToString()
+  {
+    addToEventLog("All customers: " + worker.customerQueueToString());
+    return worker.customerQueueToString();
+  }
+
+  public void initiateParcelMap(String filename)
+  {
+    try{
+      FileReader file_Reader = new FileReader(filename);
+      BufferedReader buffered_Reader = new BufferedReader(file_Reader);
+      String line;
+      while(buffered_Reader.ready()){
+          line = buffered_Reader.readLine();
+          String[] array = line.split(",");
+          parcelMap.addParcel(new Parcel(array[0], Float.parseFloat(array[1]),
+                                         Float.parseFloat(array[2]), Float.parseFloat(array[3]),
+                                         Integer.parseInt(array[4]), Float.parseFloat(array[5])));
+       }
+      buffered_Reader.close();
+      addToEventLog("Parcel collection has been initialised from file: " + filename);
+    }catch(Exception e){
+      e.printStackTrace();
+    }
+  }
+
+  public void addParcelToParcelMap(Parcel parcel)
+  {
+    addToEventLog("Parcel " + parcel.getId() + " has been added to parcel collection");
+    parcelMap.addParcel(parcel);
+  }
+
+  public String parcelMapToString()
+  {
+    addToEventLog("All parcels: " + parcelMap.toString());
+    return parcelMap.toString();
+  }
+
+  public void addToEventLog(String event)
+  {
+    log.addToEventLog(event);
+  }
+
+  public void writeToLogFile(String filename)
+  {
+    log.writeToLogFile("log.txt");
+  }
+
+  public static void main(String args[]){
+		Manager manager = new Manager();
+	}
 }
