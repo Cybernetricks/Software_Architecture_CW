4313580dc7b6ff07eda174401ba859b09b5c79a1
Cybernetricks <mcgbradley@gmail.com>
2024-12-30 03:57:07 +0000

    changed_gui_to_suit_brief_more_closely

--- src/Depot/Gui.java
+++ src/Depot/Gui.java
@@ -1,5 +1,7 @@
 package Depot;
 
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -9,9 +11,7 @@ import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.ScrollPaneConstants;
 import javax.swing.WindowConstants;
-import javax.swing.border.EmptyBorder;
-import javax.swing.border.EtchedBorder;
-import javax.swing.border.TitledBorder;
+
 
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
@@ -23,7 +23,7 @@ import java.awt.GridLayout;
 import java.awt.EventQueue;
 import java.awt.FlowLayout;
 import java.awt.Color;
-import java.awt.Frame;
+
 
 public class Gui extends JPanel implements ActionListener {
   private Manager manager;
@@ -41,28 +41,6 @@ public class Gui extends JPanel implements ActionListener {
     });
   }
 
-  private void createText(String string, String title) {
-    JPanel textPanel = new JPanel();
-    textPanel.setBorder(new TitledBorder(new EtchedBorder(), title));
-    textPanel.setBackground(new Color(255, 255, 255));
-
-    JTextArea display = new JTextArea(16, 40);
-    display.setText(string);
-    display.setBackground(new Color(255, 255, 255));
-    display.setEditable(false);
-    JScrollPane scroll = new JScrollPane(display);
-    scroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
-
-    textPanel.add(scroll);
-    Frame frame = new JFrame();
-    frame.add(textPanel);
-    frame.pack();
-
-    frame.setLocationRelativeTo(null);
-    frame.setVisible(true);
-    frame.setResizable(false);
-  }
-
   private void createGui()
   {
     JFrame frame = new JFrame("Depot GUI");
@@ -76,76 +54,62 @@ public class Gui extends JPanel implements ActionListener {
     setLayout(new BorderLayout());
 
     JLabel background = new JLabel();
-    FlowLayout fl_background = new FlowLayout(FlowLayout.LEFT);
-    fl_background.setVgap(0);
-    fl_background.setHgap(0);
-    background.setLayout(fl_background);
+    FlowLayout flowLayoutBackground = new FlowLayout(FlowLayout.LEFT);
+    flowLayoutBackground.setVgap(0);
+    flowLayoutBackground.setHgap(0);
+    background.setLayout(flowLayoutBackground);
     add(background);
 
-    JPanel buttonPanel = new JPanel(new GridLayout(11, 1));
-    buttonPanel.setBorder(new EmptyBorder(30, 30, 0, 0));
-    buttonPanel.setOpaque(false);
+    JPanel frameContainer = new JPanel();
+    frameContainer.setLayout(new BoxLayout(frameContainer, BoxLayout.X_AXIS));
+    frameContainer.setOpaque(false);
 
-    JButton initiateButton = new JButton("Initiate Parcel Collection and Customer Queue");
-    initiateButton.setPreferredSize(new Dimension(170, 45));
-    buttonPanel.add(initiateButton);
+    JPanel extraButtonPanel = new JPanel(new GridLayout(5, 1));
+    JPanel queueOfCustomersPanel = new JPanel();
+    JPanel parcelPanel = new JPanel();
+
+    extraButtonPanel.setPreferredSize(new Dimension(200, 750));
+    extraButtonPanel.setBorder(BorderFactory.createLineBorder(Color.black));
+
+    queueOfCustomersPanel.setPreferredSize(new Dimension(390, 750)); // width set at 390 for scroller
+    queueOfCustomersPanel.setLayout(new BorderLayout());
+    queueOfCustomersPanel.setBorder(BorderFactory.createLineBorder(Color.black));
+
+    JTextArea queueOfCustomersDisplay = new JTextArea();
+    queueOfCustomersDisplay.setEditable(false); // set textArea non-editable
+    JScrollPane queueOfCustomersScroll = new JScrollPane(queueOfCustomersDisplay);
+    queueOfCustomersScroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+
+    parcelPanel.setPreferredSize(new Dimension(390, 750)); // width set at 390 for scroller
+    parcelPanel.setLayout(new GridLayout(2, 1));
+    parcelPanel.setBorder(BorderFactory.createLineBorder(Color.black));
+
+    JTextArea headParcelDisplay = new JTextArea();
+    headParcelDisplay.setEditable(false);
+    headParcelDisplay.setPreferredSize(new Dimension(390, 200));
+
+    JTextArea parcelMapDisplay = new JTextArea();
+    parcelMapDisplay.setEditable(false); // set textArea non-editable
+    JScrollPane parcelMapScroll = new JScrollPane(parcelMapDisplay);
+    parcelMapScroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+
+    JButton initiateButton = new JButton("Initiate collections");
+    extraButtonPanel.add(initiateButton);
     initiateButton.addActionListener(new ActionListener() {
       public void actionPerformed(ActionEvent e) {
         manager.initiateParcelMap("Parcels.csv");
         manager.initiateCustomerQueue("Custs.csv");
-        createText("Parcel collection and Customer Queue has been initialised", "Initiate Parcel Collection and Customer Queue");
+        JOptionPane.showMessageDialog(null, "Parcel collection and Customer Queue has been initialised");
+        queueOfCustomersDisplay.setText(manager.customerQueueToString());
+        headParcelDisplay.setText(manager.getHeadParcel());
+        parcelMapDisplay.setText(manager.parcelMapToString());
       }
     });
 
-    JButton pmtsButton = new JButton("Show All Parcels");
-    pmtsButton.setPreferredSize(new Dimension(170, 45));
-    buttonPanel.add(pmtsButton);
-    pmtsButton.addActionListener(new ActionListener() {
-      public void actionPerformed(ActionEvent e) {
-        createText("id, length, width, height, weight, days in collection, collected?\n" + manager.parcelMapToString(), "List All Parcels");
-      }
-    });
-
-    JButton cqtsButton = new JButton("Show All Customers");
-    cqtsButton.setPreferredSize(new Dimension(170, 45));
-    buttonPanel.add(cqtsButton);
-    cqtsButton.addActionListener(new ActionListener() {
-      public void actionPerformed(ActionEvent e) {
-        createText("Name, Parcel ID\n" + manager.customerQueueToString(), "List All Customers");
-      }
-    });
-
-    JButton actqButton = new JButton("Add Customer To Queue");
-    actqButton.setPreferredSize(new Dimension(170, 45));
-    buttonPanel.add(actqButton);
-    actqButton.addActionListener(new ActionListener() {
-      public void actionPerformed(ActionEvent e) {
-        // Add a new JPanel to get a customer and parcel then return to be used below
-        String customerName = JOptionPane.showInputDialog("Please enter your name:");
-        String parcelId = JOptionPane.showInputDialog("Please enter your expected Parcel ID");
-        boolean result = manager.addCustomerToQueue(new Customer(customerName, parcelId));
-        if (result) {
-          createText(customerName + " has been added to queue", "Add Customer to Queue");
-        } else {
-          createText(customerName + " has not been added to queue due to parcel not being found", "Add Customer to Queue");
-        }
-      }
-    });
-
-    JButton pcciqButton = new JButton("Process Current Customer");
-    pcciqButton.setPreferredSize(new Dimension(170, 45));
-    buttonPanel.add(pcciqButton);
-    pcciqButton.addActionListener(new ActionListener() {
-      public void actionPerformed(ActionEvent e) {
-        createText("Current customer has been processed\n" + "Please pay - £" + 
-                    String.format("%.02f", manager.processCurrentCustomerInQueue()), "Process Current Customer");
-      }
-    });
-
-    JButton aptpmButton = new JButton("Add Parcel To Collection");
-    aptpmButton.setPreferredSize(new Dimension(170, 45));
-    buttonPanel.add(aptpmButton);
-    aptpmButton.addActionListener(new ActionListener() {
+    JButton addParcelButton = new JButton("Add Parcel To Collection");
+    addParcelButton.setPreferredSize(new Dimension(170, 45));
+    extraButtonPanel.add(addParcelButton);
+    addParcelButton.addActionListener(new ActionListener() {
       public void actionPerformed(ActionEvent e) {
         String parcelId = JOptionPane.showInputDialog("Please enter parcel ID:");
         float length = Float.parseFloat(JOptionPane.showInputDialog("Please enter parcel length:"));
@@ -154,14 +118,54 @@ public class Gui extends JPanel implements ActionListener {
         float weight = Float.parseFloat(JOptionPane.showInputDialog("Please enter parcel weight:"));
         int daysInCollection = Integer.parseInt(JOptionPane.showInputDialog("Please enter how many days parcel has been in collection:"));
         manager.addParcelToParcelMap(new Parcel(parcelId, length, width, height, daysInCollection, weight));
-        createText("Parcel has been created and added to collection" ,"Add Parcel To Collection");
+        JOptionPane.showMessageDialog(null, "Parcel has been created and added to collection");
+        parcelMapDisplay.setText(manager.parcelMapToString());
       }
     });
 
-    background.add(buttonPanel);
+    JButton addCustomerButton = new JButton("Add Customer To Queue");
+    addCustomerButton.setPreferredSize(new Dimension(170, 45));
+    extraButtonPanel.add(addCustomerButton);
+    addCustomerButton.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+        String customerName = JOptionPane.showInputDialog("Please enter your name:");
+        String parcelId = JOptionPane.showInputDialog("Please enter your expected Parcel ID");
+        boolean result = manager.addCustomerToQueue(new Customer(customerName, parcelId));
+        if (result) {
+          JOptionPane.showMessageDialog(null, customerName + " has been added to queue");
+          // createText(customerName + " has been added to queue", "Add Customer to Queue");
+        } else {
+          JOptionPane.showMessageDialog(null, customerName + " has not been added to queue due to parcel not being found");
+          // createText(customerName + " has not been added to queue due to parcel not being found", "Add Customer to Queue");
+        }
+        queueOfCustomersDisplay.setText(manager.customerQueueToString());
+      }
+    });
+
+    JButton processCustomerButton = new JButton("Process Current Customer");
+    extraButtonPanel.add(processCustomerButton);
+    processCustomerButton.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+        manager.processCurrentCustomerInQueue();
+        queueOfCustomersDisplay.setText(manager.customerQueueToString());
+        headParcelDisplay.setText(manager.getHeadParcel());
+        parcelMapDisplay.setText(manager.parcelMapToString());
+      }
+    });
+
+    queueOfCustomersPanel.add(queueOfCustomersScroll);
+
+    parcelPanel.add(headParcelDisplay);
+    parcelPanel.add(parcelMapScroll);
+
+    frameContainer.add(extraButtonPanel);
+    frameContainer.add(queueOfCustomersPanel);
+    frameContainer.add(parcelPanel);
+
+    background.add(frameContainer);
 
     frame.getContentPane().add(this);
-    frame.setSize(900, 600);
+    frame.setSize(1000, 800);
 
     frame.setLocationByPlatform(true);
     frame.setVisible(true);
